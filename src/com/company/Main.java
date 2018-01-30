c2f4c9c2a99701053bb53fb15f0ab688c0d30296
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-30 16:17:36 +0200

    Code 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,8 @@ public class Main {
         sandauga(a, b);
 
     }
-    public static void duomenys (){
+
+    public static void duomenys() {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Ivesk du skaicius: ");
@@ -27,13 +28,16 @@ public class Main {
         System.out.println("Atsakymai: " + skirtumas(a, b));
         System.out.println("Atsakymai: " + sandauga(a, b));
     }
-    public static float suma (float a, float b){
+
+    public static float suma(float a, float b) {
         return a + b;
     }
-    public static float skirtumas (float a, float b){
+
+    public static float skirtumas(float a, float b) {
         return a - b;
     }
-    public static float sandauga (float a, float b){
+
+    public static float sandauga(float a, float b) {
         return a * b;
     }
 }
